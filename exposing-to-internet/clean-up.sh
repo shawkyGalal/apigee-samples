07f675f9c4212e58bccad625298f257297c157be
shawkyGalal <shawky.foda@gmail.com>
2024-03-05 13:32:50 +0300

    User Service Account To Authenticate clean-up.sh
    User Service Account To Authenticate clean-up.sh
    Use Command Line Arguments as in the deploy.sh

--- exposing-to-internet/clean-up.sh
+++ exposing-to-internet/clean-up.sh
@@ -14,6 +14,38 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+NETWORK="default"
+SUBNET="default"
+# CRED_FILE="./service-account-keys/jenkins@moj-prod-apigee.iam.gserviceaccount.com.json"
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+	key="$1"
+	case $key in
+		--CRED_FILE)
+			CRED_FILE="$2"
+			echo "====CRED_FILE : $CRED_FILE ======="
+			shift # past argument
+			shift # past value
+			;;              
+
+		--ENV_NAME)
+			 ENV_NAME="$2"
+			echo "====ENV_NAME : $ENV_NAME ======="
+			shift # past argument
+			shift # past value
+			;;              
+		            
+	esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+PROJECT=$(cat "$CRED_FILE" | jq --raw-output '.project_id')
+echo "================================================================================================="
+echo "=== Start Deleting Environment $ENV_NAME  on Google Cloud Project $PROJECT ===========" 
+echo "================================================================================================="
+
 if [ -z "$PROJECT" ]; then
   echo "No PROJECT variable set"
   exit
@@ -25,6 +57,9 @@ if ! [ -x "$(command -v jq)" ]; then
 fi
 
 function wait_for_operation() {
+
+if [ ! -z "$1" ]; then 
+ echo "Wating for operation $1 : "
   while true; do
     STATE="$(apigeecli operations get -o "$PROJECT" -n "$1" -t "$TOKEN" | jq --raw-output '.metadata.state')"
     if [ "$STATE" = "FINISHED" ]; then
@@ -34,7 +69,11 @@ function wait_for_operation() {
     echo -n .
     sleep 5
   done
+ fi
 }
+
+gcloud auth login --cred-file $CRED_FILE
+gcloud config set project $PROJECT
 
 echo "Installing apigeecli"
 curl -s https://raw.githubusercontent.com/apigee/apigeecli/main/downloadLatest.sh | bash
@@ -46,46 +85,54 @@ TOKEN="$(gcloud auth print-access-token)"
 INSTANCE_JSON=$(apigeecli instances list -o "$PROJECT" -t "$TOKEN")
 INSTANCE_NAME=$(echo "$INSTANCE_JSON" | jq --raw-output '.instances[0].name')
 REGION=$(echo "$INSTANCE_JSON" | jq --raw-output '.instances[0].location')
-ENVIRONMENT_NAME="sample-environment"
-ENVIRONMENT_GROUP_NAME="sample-environment-group"
+    
+#"$ENVIRONMENT_GROUP_NAME".$(echo "$RUNTIME_IP" | tr '.' '-').nip.io
+ENVIRONMENT_GROUP_NAME=$ENV_NAME"-group"
+LOAD_BLANCER_VIP_NAME=$ENV_NAME"-vip"
+SSL_CERTIFICATE_NAME=$ENV_NAME"-ssl-cert"
+NETWORK_ENDPOINT_GROUP=$ENV_NAME"-neg"
+BACKEND_SERVICE_NAME=$ENV_NAME"-backend-service"
+URL_MAP_NAME=$ENV_NAME-"urlmap"
+TARGET_HTTPS_PROXY_NAME=$ENV_NAME"-https-proxy"
+FORWARDING_RULE_NAME=$ENV_NAME"-https-lb-rule"
 
 echo "Deleting load balancer..."
 # Delete forwarding rule
-gcloud compute forwarding-rules delete sample-apigee-https-lb-rule \
+gcloud compute forwarding-rules delete $FORWARDING_RULE_NAME \
   --global \
   --project="$PROJECT" --quiet
 
 # Delete target HTTPS proxy
-gcloud compute target-https-proxies delete sample-apigee-https-proxy \
+gcloud compute target-https-proxies delete $TARGET_HTTPS_PROXY_NAME \
   --project="$PROJECT" --quiet
 
 # Delete URL map
-gcloud compute url-maps delete sample-apigee-urlmap \
+gcloud compute url-maps delete $URL_MAP_NAME \
   --project="$PROJECT" --quiet
 
 # Delete backend service
-gcloud compute backend-services delete sample-apigee-backend \
+gcloud compute backend-services delete $BACKEND_SERVICE_NAME \
   --global \
   --project="$PROJECT" --quiet
 
 # Delete NEG
-gcloud compute network-endpoint-groups delete sample-apigee-neg \
+gcloud compute network-endpoint-groups delete $NETWORK_ENDPOINT_GROUP \
   --region="$REGION" \
   --project="$PROJECT" --quiet
 
 # Delete cert
 echo "Deleting SSL certificate..."
-gcloud compute ssl-certificates delete sample-apigee-ssl-cert \
+gcloud compute ssl-certificates delete $SSL_CERTIFICATE_NAME \
   --project "$PROJECT" --quiet
 
 # Delete VIP
 echo "Deleting load balancer IP address..."
-gcloud compute addresses delete sample-apigee-vip \
+gcloud compute addresses delete $LOAD_BLANCER_VIP_NAME \
   --global \
   --project "$PROJECT" --quiet
 
 echo -n "Detaching environment from group..."
-OPERATION=$(apigeecli envgroups detach -o "$PROJECT" -e "$ENVIRONMENT_NAME" -n "$ENVIRONMENT_GROUP_NAME" -t "$TOKEN" | jq --raw-output '.name' | awk -F/ '{print $4}')
+OPERATION=$(apigeecli envgroups detach -o "$PROJECT" -e "$ENV_NAME" -n "$ENVIRONMENT_GROUP_NAME" -t "$TOKEN" | jq --raw-output '.name' | awk -F/ '{print $4}')
 wait_for_operation "$OPERATION"
 
 echo -n "Deleting environment group..."
@@ -95,11 +142,11 @@ OPERATION=$(curl -X DELETE "https://apigee.googleapis.com/v1/organizations/$PROJ
 wait_for_operation "$OPERATION"
 
 echo -n "Detaching environment from instance..."
-OPERATION=$(apigeecli instances attachments detach -o "$PROJECT" -e "$ENVIRONMENT_NAME" -n "$INSTANCE_NAME" -t "$TOKEN" | jq --raw-output '.name' | awk -F/ '{print $4}')
+OPERATION=$(apigeecli instances attachments detach -o "$PROJECT" -e "$ENV_NAME" -n "$INSTANCE_NAME" -t "$TOKEN" | jq --raw-output '.name' | awk -F/ '{print $4}')
 wait_for_operation "$OPERATION"
 
 echo -n "Deleting environment..."
-OPERATION=$(apigeecli environments delete -o "$PROJECT" -e "$ENVIRONMENT_NAME" -t "$TOKEN" | jq --raw-output '.name' | awk -F/ '{print $4}')
+OPERATION=$(apigeecli environments delete -o "$PROJECT" -e "$ENV_NAME" -t "$TOKEN" | jq --raw-output '.name' | awk -F/ '{print $4}')
 wait_for_operation "$OPERATION"
 
 echo "Clean up complete!"
