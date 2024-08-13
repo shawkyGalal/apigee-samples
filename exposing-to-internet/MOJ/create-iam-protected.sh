6c7d6918f732529771568d45b26c4b0323590ae9
shawkyGalal <shawky.foda@gmail.com>
2024-08-13 16:58:22 +0300

    Minor Updates

--- exposing-to-internet/MOJ/create-iam-protected.sh
+++ exposing-to-internet/MOJ/create-iam-protected.sh
@@ -4,9 +4,9 @@ KEY_FILE="/etc/apigee/gitRepos/apigee-samples/exposing-to-internet/MOJ/service-a
 PROJECT="moj-prod-apigee"
 ENV_NAME=iam-protected
 ENV_GROUP_DNS=apis.moj.gov.sa
-TLS_CERT_PATH="/etc/apigee/gitRepos/apigee-samples/exposing-to-internet/MOJ/Certificates/2024/te_cb83fa55_4c30_45ee_93e4_b51dd9e5f992.cer"
-TLS_KEY_PATH="/etc/apigee/gitRepos/apigee-samples/exposing-to-internet/MOJ/Certificates/2024/te_cb83fa55_4c30_45ee_93e4_b51dd9e5f992.key"
+TLS_CERT_PATH="./Certificates/2024/te_cb83fa55_4c30_45ee_93e4_b51dd9e5f992.cer"
+TLS_KEY_PATH="./Certificates/2024/te_cb83fa55_4c30_45ee_93e4_b51dd9e5f992.key"
 
 
 
-"./create-env.sh" --AUTH_METHOD  USER_SESSION --PROJECT $$PROJECT --KEY_FILE $KEY_FILE --ENV_NAME $ENV_NAME --ENV_GROUP_DNS $ENV_GROUP_DNS --TLS_CERT_PATH $TLS_CERT_PATH  --TLS_KEY_PATH $TLS_KEY_PATH
+"./create-env.sh" --AUTH_METHOD  USER_SESSION --PROJECT $PROJECT --KEY_FILE $KEY_FILE --ENV_NAME $ENV_NAME --ENV_GROUP_DNS $ENV_GROUP_DNS --TLS_CERT_PATH $TLS_CERT_PATH  --TLS_KEY_PATH $TLS_KEY_PATH
--- exposing-to-internet/MOJ/delete-iam-protected.sh
+++ exposing-to-internet/MOJ/delete-iam-protected.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-KEY_FILE="/etc/apigee/gitRepos/apigee-samples/exposing-to-internet/MOJ/service-account-keys/jenkins@moj-prod-apigee.iam.gserviceaccount.com.json"
+PROJECT="moj-prod-apigee"
+KEY_FILE="./service-account-keys/jenkins@moj-prod-apigee.iam.gserviceaccount.com.json"
 ENV_NAME="iam-protected"
 
-
-"./clean-up.sh" --AUTH_METHOD  USER_SESSION --ENV_NAME $ENV_NAME
+"./clean-up.sh" --AUTH_METHOD USER_SESSION --PROJECT $PROJECT --ENV_NAME $ENV_NAME
